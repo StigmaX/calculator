fcf9a63e06e952d1495bb4753fd53330f36cf943
Wildan Kusnaedi <Wildank40@gmail.com>
2023-04-13 20:54:10 +0700

    Add function for button clear, calculate, and minus. Fixed bug on plus button(working even with empty field)

--- script/script.js
+++ script/script.js
@@ -5,6 +5,8 @@ const numberAddition = document.querySelector('#plus');
 const numberDivision = document.querySelector('#divide');
 const numberMultiplication = document.querySelector('#multiply');
 const numberMinus = document.querySelector('#minus');
+const numberDel = document.querySelector('#del');
+const numberClear = document.querySelector('#clear');
 const calculateNumber = document.querySelector('#calculate');
 let lastResult=0;
 numberBtn.forEach((button) => {
@@ -25,23 +27,43 @@ numberBtn.forEach((button) => {
 
 calculateNumber.addEventListener('click', ()=>{
     if(currentNumber.textContent==0 || lastNumber.textContent==0)return;
+    lastNumber.textContent = eval(currentNumber.textContent);;
     console.log(eval(currentNumber.textContent));
-    currentNumber.textContent = +lastResult + +currentNumber.textContent;
+    currentNumber.textContent = 0;
+})
+
+numberDel.addEventListener('click', ()=>{
+    const del = currentNumber.textContent.slice(0, -1);
+    currentNumber.textContent = del;
+})
+
+numberClear.addEventListener('click', ()=>{
+    currentNumber.textContent = 0;
     lastNumber.textContent = 0;
+    lastResult = 0;
 })
 
 numberAddition.addEventListener('click', ()=> {
+    if(currentNumber.textContent=='') return;
     if(lastNumber.textContent == '0'){
         lastNumber.textContent = currentNumber.textContent;
-        lastNumber.textContent;
+        lastResult = currentNumber.textContent;
     }
-    else lastNumber.textContent += '+'+currentNumber.textContent;
+    else {lastNumber.textContent += '+'+currentNumber.textContent;
     const result = +currentNumber.textContent + +lastResult;
     lastResult = result;
-    currentNumber.textContent = result;
+    currentNumber.textContent = result;}
 })
 numberMinus.addEventListener('click', ()=>{
-    currentNumber.textContent+= '-';
+    if(currentNumber.textContent=='') return;
+    if(lastNumber.textContent == '0'){
+        lastNumber.textContent = currentNumber.textContent;
+        lastResult = currentNumber.textContent;
+    }
+    else {lastNumber.textContent += '-'+currentNumber.textContent;
+    const result = +lastResult - +currentNumber.textContent;
+    lastResult = result;
+    currentNumber.textContent = result;}
 })
 numberMultiplication.addEventListener('click', ()=>{
     currentNumber.textContent+= '*';
